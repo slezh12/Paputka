3ac390c68cd4ea99424ca4437325163156fcb6c7
GiorgiGagnidze <PRO@MACs-MacBook-Pro-4.local>
2014-05-31 00:17:28 +0400

    ContextListeneri da Connectionpooli movagvare
    MyDBInfo shi mititebulia default informacia. anu account aris root da
    paswordi "". tu tqveni accounti ar shegiqmniat bazaze mashin gaeshveba

---
+++ src/JavaPackage/ContextListener.java
@@ -0,0 +1,51 @@
+package JavaPackage;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
+
+import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
+
+
+/**
+ * Application Lifecycle Listener implementation class ContextListener
+ *
+ */
+@WebListener
+public class ContextListener implements ServletContextListener {
+
+	static String account = MyDBInfo.MYSQL_USERNAME;
+	static String password = MyDBInfo.MYSQL_PASSWORD;
+	static String server = MyDBInfo.MYSQL_DATABASE_SERVER;
+	static String database = MyDBInfo.MYSQL_DATABASE_NAME;
+	
+    /**
+     * Default constructor. 
+     */
+    public ContextListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0) {
+    	DataSource source = new BasicDataSource();
+    	((BasicDataSource) source).setDriverClassName("com.mysql.jdbc.Driver");
+    	((BasicDataSource) source).setUsername(account);
+    	((BasicDataSource) source).setPassword(password);
+    	((BasicDataSource) source).setUrl("jdbc:mysql://"+server+":3306/"+database);
+    	ServletContext context = arg0.getServletContext();
+        context.setAttribute("connectionPool", source);
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+	
+}
---
+++ src/JavaPackage/MyDBInfo.java
@@ -0,0 +1,9 @@
+package JavaPackage;
+
+//This is mysql server information.
+public class MyDBInfo {
+	public static final String MYSQL_USERNAME = "root";
+	public static final String MYSQL_PASSWORD = "";
+	public static final String MYSQL_DATABASE_SERVER = "localhost";
+	public static final String MYSQL_DATABASE_NAME = "paputka";
+}
