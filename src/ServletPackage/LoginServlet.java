d47d426c362368bd683428db070014daf5495956
GiorgiGagnidze <PRO@MACs-MacBook-Pro-4.local>
2014-06-01 01:33:21 +0400

    baza gaitesta vagrdzeleb loginservletis weras

--- src/ServletPackage/LoginServlet.java
+++ src/ServletPackage/LoginServlet.java
@@ -1,11 +1,21 @@
 package ServletPackage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import JavaPackage.MyDBInfo;
+
+
 
 /**
  * Servlet implementation class LoginServlet
@@ -33,7 +43,17 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String name = request.getParameter("name");
+		String password = request.getParameter("pass");
+		ServletContext context = getServletContext();
+		DataSource source = (DataSource) context.getAttribute("connectionPool");
+		try {
+			Connection con = source.getConnection();
+			Statement stmt = con.createStatement();
+			stmt.executeQuery("USE " + MyDBInfo.MYSQL_DATABASE_NAME);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
