cee802428400858a13be88271fdd11066b945dd2
abakhso <achi_baxsoliani@yahoo.com>
2014-06-08 17:43:31 +0400

    BaseConnection Test.   mushaobs ukve.
    sheidzleba sxvebis damatebac

--- src/JavaPackage/BaseConnectionTest.java
+++ src/JavaPackage/BaseConnectionTest.java
@@ -7,12 +7,29 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.servlet.ServletContext;
+import javax.sql.DataSource;
 
+import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BaseConnectionTest {
 	
+	static String account = MyDBInfo.MYSQL_USERNAME;
+	static String password = MyDBInfo.MYSQL_PASSWORD;
+	static String server = MyDBInfo.MYSQL_DATABASE_SERVER;
+	static String database = MyDBInfo.MYSQL_DATABASE_NAME;
 	
+	private DataSource source;
+	
+	@Before
+	public void SetUp(){
+		source = new BasicDataSource();
+    	((BasicDataSource) source).setDriverClassName("com.mysql.jdbc.Driver");
+    	((BasicDataSource) source).setUsername(account);
+    	((BasicDataSource) source).setPassword(password);
+    	((BasicDataSource) source).setUrl("jdbc:mysql://"+server+":3306/"+database);
+	}
 	
 	/* gaketebulia
 	 * insert into users (FirstName, LastName, Gender, BirthDate,  Password, EMail)
@@ -20,8 +37,7 @@ values (N'achi', N'baxlosania', true, '1994-08-23' ,  N'40bd001563085fc35165329e
 	 */
 	@Test
 	public void test() {
-		/*
-		BaseConnection base = new BaseConnection(context);
+		BaseConnection base = new BaseConnection((BasicDataSource) source);
 		try {
 			ResultSet rs = base.getInfoByMail("achi_baxlosania@yahoo.com");
 			if (rs.isBeforeFirst()) {
@@ -36,7 +52,7 @@ values (N'achi', N'baxlosania', true, '1994-08-23' ,  N'40bd001563085fc35165329e
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		*/
+		
 	}
 	
 }
