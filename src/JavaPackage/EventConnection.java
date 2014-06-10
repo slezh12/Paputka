205cfab0061d1656b9b7c8e52c2fb2e06fdb13ee
slezh12 <slezh12@freeuni.edu.ge>
2014-06-10 20:17:58 +0300

    eventconnection srulad

--- src/JavaPackage/EventConnection.java
+++ src/JavaPackage/EventConnection.java
@@ -1,5 +1,7 @@
 package JavaPackage;
 
+import java.sql.SQLException;
+
 import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
 
 public class EventConnection extends BaseConnection {
@@ -8,30 +10,102 @@ public class EventConnection extends BaseConnection {
 		super(source);
 	}
 
-	public void insertIntoRequestss(int userID, int places, double fee,
+	public void insertIntoEvents(int userID, int places, double fee,
 			double FromLongitude, double FromLatitude, double ToLongitude,
 			double ToLatitude, String fromPlace, String toPlace, boolean type,
 			boolean validation) {
-		// code here
+		try {
+			super.stmt
+					.executeUpdate("INSERT INTO Events (userID,places,fee,FromLongitude,FromLatitude,ToLongitude,ToLatitude,fromPlace,toPlace,type,validation) VALUES("
+							+ userID
+							+ ","
+							+ places
+							+ ","
+							+ fee
+							+ ","
+							+ FromLongitude
+							+ ","
+							+ FromLatitude
+							+ ","
+							+ ToLongitude
+							+ ","
+							+ ToLatitude
+							+ ","
+							+ "'"
+							+ fromPlace
+							+ "'"
+							+ ","
+							+ "'"
+							+ toPlace
+							+ "'"
+							+ ","
+							+ type + "," + validation + ")");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public void insertIntoDates(int eventID, String date) {
-		// code here
+		try {
+			super.stmt
+					.executeUpdate("INSERT INTO Dates (eventID , date) VALUES("
+							+ eventID + "," + "'" + date + "'" + ")");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	public void insertIntoEveryday(int eventID, String sartTime, int day) {
-		// code here
+
+	public void insertIntoEveryday(int eventID, String startTime, int day) {
+		try {
+			super.stmt
+					.executeUpdate("INSERT INTO Everyday (eventID,startTime,day) VALUES("
+							+ eventID
+							+ ","
+							+ "'"
+							+ startTime
+							+ "'"
+							+ ","
+							+ day
+							+ ")");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public void insertIntoParticipants(int eventID, int userID) {
-		// code here
+		try {
+			super.stmt
+					.executeUpdate("INSERT INTO Participants (eventID,userID) VALUES("
+							+ eventID + "," + userID + ")");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	public void insertIntoComments(int eventID, int userID, String comment,String datetime) {
-		// code here
+
+	public void insertIntoComments(int eventID, int userID, String comment,
+			String datetime) {
+		try {
+			super.stmt
+					.executeUpdate("INSERT INTO Comments (eventID,userID,comment,dateTime) VALUES("
+							+ eventID
+							+ ","
+							+ userID
+							+ ","
+							+ "'"
+							+ comment
+							+ "'" + "," + "'" + datetime + "'" + ")");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public void updateEvents(int eventID, boolean validation) {
-		// code here
+		try {
+			super.stmt.executeUpdate("UPDATE Events Set validation = "
+					+ validation + "WHERE ID = " + eventID);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
