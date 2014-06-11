41c00c055cf08515853725bd34ed7a1b9843a80f
GiorgiGagnidze <PRO@MACs-MacBook-Pro-4.local>
2014-06-12 00:59:21 +0400

    paswordi

--- src/JavaPackage/MyDBInfo.java
+++ src/JavaPackage/MyDBInfo.java
@@ -3,11 +3,7 @@ package JavaPackage;
 //This is mysql server information.
 public class MyDBInfo {
 	public static final String MYSQL_USERNAME = "root";
-<<<<<<< HEAD
 	public static final String MYSQL_PASSWORD = "";
-=======
-	public static final String MYSQL_PASSWORD = "archila123";
->>>>>>> FETCH_HEAD
 	public static final String MYSQL_DATABASE_SERVER = "localhost";
 	public static final String MYSQL_DATABASE_NAME = "paputka";
 }
