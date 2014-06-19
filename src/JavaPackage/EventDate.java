b4c47da73576bd9a7afa64701a51dc66d6f7dc42
abakhso <achi_baxsoliani@yahoo.com>
2014-06-19 23:38:39 +0400

    Create EventDate.java

---
+++ src/JavaPackage/EventDate.java
@@ -0,0 +1,23 @@
+package JavaPackage;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+
+public class EventDate {
+	private Time data;
+	private int day;
+	public EventDate(int day, Time date){
+		this.day = day;
+		this.data = date;
+	}
+	
+	public int getDay(){
+		return this.day;
+	}
+	
+	public Time getDate(){
+		return this.data;
+	}
+	
+	
+}
