43c5c129fc12737adf6599672f74531dd52f389e
BeqaKhaburdzania <bkhab12@freeuni.edu.ge>
2014-06-23 16:44:52 +0400

    davamate testebi

---
+++ src/JavaPackage/EventParseInfoTest.java
@@ -0,0 +1,14 @@
+package JavaPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EventParseInfoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
