d95124c25e71944da253c2fe19742c1e0d761e9b
BeqaKhaburdzania <bkhab12@freeuni.edu.ge>
2014-06-25 10:11:44 +0400

    testebi

--- src/JavaPackage/WantToGoParseInfoTest.java
+++ src/JavaPackage/WantToGoParseInfoTest.java
@@ -3,6 +3,7 @@ package JavaPackage;
 import static org.junit.Assert.*;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -38,25 +39,42 @@ public class WantToGoParseInfoTest {
 	
 	@Test
 	public void testGetLastID() {
-		
+		Date date = Date.valueOf("1994-02-04");
 		
 		boolean type = false;
 		try {
 			Connection con = source.getConnection();
 			Statement stmt = con.createStatement();
 			stmt.executeQuery("USE " + database);
-			stmt.executeUpdate("INSERT INTO Users (FirstName, LastName, Gender, Password, EMail) Values ('tedo' ,'chubo', true, '123', 'tedo')");
-			int userID = 0;
-			int EventID = 0;
-			ResultSet rs = stmt
-					.executeQuery("SELECT ID FROM Users Where EMail = 'tedo'");
-			if (rs.isBeforeFirst()) {
-				rs.next();
-				userID = rs.getInt("ID");
-			}
+			
+			stmt.executeUpdate("INSERT INTO Users (FirstName , LastName, EMail , Password , Gender, BirthDate) VALUES("
+					+ "'"
+					+ "user1"
+					+ "'"
+					+ ","
+					+ "'"
+					+ "ar vici"
+					+ "'"
+					+ ","
+					+ "'"
+					+ "asaaaaaasdpsdasd,mjniaj"
+					+ "'"
+					+ ","
+					+ "'"
+					+ "password"
+					+ "'"
+					+ ","
+					+ true + "," + "'" + date + "'" + ")");
+			int lastID1 = 0;
+			ResultSet res1 = stmt.executeQuery("SELECT * FROM Users ORDER BY ID DESC LIMIT 1");
+			while(res1.next()){
+				 lastID1 = res1.getInt("ID");
+				
+			}	
+			
 			stmt.executeQuery("USE " + database);
 			stmt.executeUpdate("INSERT INTO WantToGo (UserID , title, FromLongitude , FromLatitude ,ToLongitude, ToLatitude , Type) VALUES("
-					+ userID
+					+ lastID1
 					+ ","
 					+ "'"
 					+ "bla"
@@ -79,11 +97,11 @@ public class WantToGoParseInfoTest {
 				
 			}	
 			WantToGoParseInfo temp = new WantToGoParseInfo((BasicDataSource) source);
-			assertEquals(lastID, temp.getLastID(userID));
-			assertFalse(isEqual(-1, temp.getLastID(userID)));
+			assertEquals(lastID, temp.getLastID(lastID1));
+			assertFalse(isEqual(-1, temp.getLastID(lastID1)));
 			
-			stmt.executeUpdate("DELETE FROM WantToGo where UserID = " + userID);
-			stmt.executeUpdate("DELETE FROM Users where ID = " + userID);
+			stmt.executeUpdate("DELETE FROM WantToGo where ID = " + lastID);
+			stmt.executeUpdate("DELETE FROM Users where ID = " + lastID1);
 		//	stmt.executeUpdate("DELETE FROM WantToGO WHERE UserID  = "
 		//				+ 1);
 			
@@ -93,32 +111,58 @@ public class WantToGoParseInfoTest {
 		}
 	}
 	
-	// checks if two ints equla to each other
+	// checks if two ints equals to each other
 	private boolean isEqual(double first, double second) {
 		return (first == second);
 	}
 	
 	@Test
 	public void testgetWantToGos() {
-		
+		Date date = Date.valueOf("1994-02-04");
 		
 		boolean type = false;
 		try {
 			Connection con = source.getConnection();
 			Statement stmt = con.createStatement();
 			stmt.executeQuery("USE " + database);
-			stmt.executeUpdate("INSERT INTO Users (FirstName, LastName, Gender, Password, EMail) Values ('tedo' ,'chubo', true, '123', 'tedo')");
-			int userID = 0;
-			int EventID = 0;
-			ResultSet rs = stmt
-					.executeQuery("SELECT ID FROM Users Where EMail = 'tedo'");
-			if (rs.isBeforeFirst()) {
-				rs.next();
-				userID = rs.getInt("ID");
-			}
+//			stmt.executeUpdate("INSERT INTO Users (FirstName, LastName, Gender, Password, EMail) Values ('tedo' ,'chubo', true, '123', 'tedo')");
+//			int userID = 0;
+//			int EventID = 0;
+//			ResultSet rs = stmt
+//					.executeQuery("SELECT ID FROM Users Where EMail = 'tedo'");
+//			if (rs.isBeforeFirst()) {
+//				rs.next();
+//				userID = rs.getInt("ID");
+//			}
+			
+			stmt.executeUpdate("INSERT INTO Users (FirstName , LastName, EMail , Password , Gender, BirthDate) VALUES("
+					+ "'"
+					+ "user1"
+					+ "'"
+					+ ","
+					+ "'"
+					+ "ar vici"
+					+ "'"
+					+ ","
+					+ "'"
+					+ "mnmnmj"
+					+ "'"
+					+ ","
+					+ "'"
+					+ "password"
+					+ "'"
+					+ ","
+					+ true + "," + "'" + date + "'" + ")");
+			int lastID1 = 0;
+			ResultSet res1 = stmt.executeQuery("SELECT * FROM Users ORDER BY ID DESC LIMIT 1");
+			while(res1.next()){
+				 lastID1 = res1.getInt("ID");
+				
+			}	
+			
 			stmt.executeQuery("USE " + database);
 			stmt.executeUpdate("INSERT INTO WantToGo (UserID , title, FromLongitude , FromLatitude ,ToLongitude, ToLatitude , Type) VALUES("
-					+ userID
+					+ lastID1
 					+ ","
 					+ "'"
 					+ "bla"
@@ -135,7 +179,7 @@ public class WantToGoParseInfoTest {
 					+ type +")");
 			
 			stmt.executeUpdate("INSERT INTO WantToGo (UserID , title, FromLongitude , FromLatitude ,ToLongitude, ToLatitude , Type) VALUES("
-					+ userID
+					+ lastID1
 					+ ","
 					+ "'"
 					+ "bla2"
@@ -153,11 +197,11 @@ public class WantToGoParseInfoTest {
 			
 			
 			WantToGoParseInfo temp = new WantToGoParseInfo((BasicDataSource) source);
-			assertEquals(2, temp.getWantToGos(userID).size());
-			assertFalse(isEqual(3, temp.getLastID(userID)));
+			assertEquals(2, temp.getWantToGos(lastID1).size());
+			assertFalse(isEqual(3, temp.getLastID(lastID1)));
 			
-			stmt.executeUpdate("DELETE FROM WantToGo Where UserID = " + userID);
-			stmt.executeUpdate("DELETE FROM Users where ID = " + userID);
+			stmt.executeUpdate("DELETE FROM WantToGo Where UserID = " + lastID1);
+			stmt.executeUpdate("DELETE FROM Users where ID = " + lastID1);
 
 		//	stmt.executeUpdate("DELETE FROM WantToGO WHERE UserID  = "
 		//				+ 1);
@@ -170,26 +214,50 @@ public class WantToGoParseInfoTest {
 	
 	@Test
 	public void testgetWantToGos2() {
-		
+		Date date = Date.valueOf("1994-02-04");
 		boolean type = false;
 		try {
 			Connection con = source.getConnection();
 			Statement stmt = con.createStatement();
 			stmt.executeQuery("USE " + database);
-			stmt.executeUpdate("INSERT INTO Users (FirstName, LastName, Gender, Password, EMail) Values ('tedo' ,'chubo', true, '123', 'tedo')");
-			int userID = 0;
-			int EventID = 0;
-			ResultSet rs = stmt
-					.executeQuery("SELECT ID FROM Users Where EMail = 'tedo'");
-			if (rs.isBeforeFirst()) {
-				rs.next();
-				userID = rs.getInt("ID");
-			}
-			stmt.executeQuery("USE " + database);
+//			stmt.executeUpdate("INSERT INTO Users (FirstName, LastName, Gender, Password, EMail) Values ('tedo' ,'chubo', true, '123', 'tedo')");
+//			int userID = 0;
+//			int EventID = 0;
+//			ResultSet rs = stmt
+//					.executeQuery("SELECT ID FROM Users Where EMail = 'tedo'");
+//			if (rs.isBeforeFirst()) {
+//				rs.next();
+//				userID = rs.getInt("ID");
+//			}
+//			stmt.executeQuery("USE " + database);
 			
+			stmt.executeUpdate("INSERT INTO Users (FirstName , LastName, EMail , Password , Gender, BirthDate) VALUES("
+					+ "'"
+					+ "user1"
+					+ "'"
+					+ ","
+					+ "'"
+					+ "ar vici"
+					+ "'"
+					+ ","
+					+ "'"
+					+ "asaasdadbasadsfaaaplj"
+					+ "'"
+					+ ","
+					+ "'"
+					+ "password"
+					+ "'"
+					+ ","
+					+ true + "," + "'" + date + "'" + ")");
+			int lastID1 = 0;
+			ResultSet res1 = stmt.executeQuery("SELECT * FROM Users ORDER BY ID DESC LIMIT 1");
+			while(res1.next()){
+				 lastID1 = res1.getInt("ID");
+				
+			}	
 			
 			stmt.executeUpdate("INSERT INTO WantToGo (UserID , title, FromLongitude , FromLatitude ,ToLongitude, ToLatitude , Type) VALUES("
-					+ userID
+					+ lastID1
 					+ ","
 					+ "'"
 					+ "bla2"
@@ -208,13 +276,13 @@ public class WantToGoParseInfoTest {
 			
 			
 			WantToGoParseInfo temp = new WantToGoParseInfo((BasicDataSource) source);
-			assertEquals(1, temp.getWantToGos(userID).size());
-			assertEquals(userID, temp.getWantToGos(userID).get(0).getUserID());
-			assertTrue(temp.getWantToGos(userID).get(0).getTitle().equals("bla2"));
+			assertEquals(1, temp.getWantToGos(lastID1).size());
+			assertEquals(lastID1, temp.getWantToGos(lastID1).get(0).getUserID());
+			assertTrue(temp.getWantToGos(lastID1).get(0).getTitle().equals("bla2"));
 			assertFalse(type);
 			
-			stmt.executeUpdate("DELETE FROM WantToGo where UserID = " + userID);
-			stmt.executeUpdate("DELETE FROM Users where ID = " + userID);
+			stmt.executeUpdate("DELETE FROM WantToGo where UserID = " + lastID1);
+			stmt.executeUpdate("DELETE FROM Users where ID = " + lastID1);
 				
 		//	stmt.executeUpdate("DELETE FROM WantToGO WHERE UserID  = "
 		//				+ 1);
@@ -227,22 +295,51 @@ public class WantToGoParseInfoTest {
 	
 	@Test
 	public void testGetWantToGo(){
+		Date date = Date.valueOf("1994-02-04");
+
 		try {
 			Connection con = source.getConnection();
 			Statement stmt = con.createStatement();
 			stmt.executeQuery("USE " + database);
-			stmt.executeUpdate("INSERT INTO Users (FirstName, LastName, Gender, Password, EMail) Values ('tedo' ,'chubo', true, '123', 'tedo')");
-			int userID = 0;
-			int EventID = 0;
-			ResultSet rs = stmt
-					.executeQuery("SELECT ID FROM Users Where EMail = 'tedo'");
-			if (rs.isBeforeFirst()) {
-				rs.next();
-				userID = rs.getInt("ID");
-			}
+//			stmt.executeUpdate("INSERT INTO Users (FirstName, LastName, Gender, Password, EMail) Values ('tedo' ,'chubo', true, '123', 'tedoo')");
+//			int userID = 0;
+//			int EventID = 0;
+//			ResultSet rs = stmt
+//					.executeQuery("SELECT ID FROM Users Where EMail = 'tedoo'");
+//			if (rs.isBeforeFirst()) {
+//				rs.next();
+//				userID = rs.getInt("ID");
+//			}
+			
+			stmt.executeUpdate("INSERT INTO Users (FirstName , LastName, EMail , Password , Gender, BirthDate) VALUES("
+					+ "'"
+					+ "user1"
+					+ "'"
+					+ ","
+					+ "'"
+					+ "ar vici"
+					+ "'"
+					+ ","
+					+ "'"
+					+ "asaasdaaassdasdaj"
+					+ "'"
+					+ ","
+					+ "'"
+					+ "password"
+					+ "'"
+					+ ","
+					+ true + "," + "'" + date + "'" + ")");
+			int lastID1 = 0;
+			ResultSet res1 = stmt.executeQuery("SELECT * FROM Users ORDER BY ID DESC LIMIT 1");
+			while(res1.next()){
+				 lastID1 = res1.getInt("ID");
+				
+			}	
+			
+			
 			stmt.executeQuery("USE " + database);
 			stmt.executeUpdate("INSERT INTO WantToGo (UserID , title, FromLongitude , FromLatitude ,ToLongitude, ToLatitude , Type) VALUES("
-					+ userID
+					+ lastID1
 					+ ","
 					+ "'"
 					+ "bla"
@@ -264,9 +361,9 @@ public class WantToGoParseInfoTest {
 				
 			}	
 			WantToGoParseInfo temp = new WantToGoParseInfo((BasicDataSource) source);
-			WantToGo t = temp.getWantToGo(lastID, userID);
+			WantToGo t = temp.getWantToGo(lastID, lastID1);
 			assertEquals(t.getID(), lastID);
-			assertEquals(t.getUserID(), userID);
+			assertEquals(t.getUserID(), lastID1);
 			assertEquals(t.getTitle(), "bla");
 			assertEquals(t.getType(), false);
 			assertEquals(isEqual(t.getFromLatitude(),0), true);
@@ -274,13 +371,15 @@ public class WantToGoParseInfoTest {
 			assertEquals(isEqual(t.getToLatitude(),0), true);
 			assertEquals(isEqual(t.getToLongitude(),0), true);
 			
-			stmt.executeUpdate("DELETE FROM WantToGo where UserID = " + userID);
-			stmt.executeUpdate("DELETE FROM Users where ID = " + userID);
+			stmt.executeUpdate("DELETE FROM WantToGo where UserID = " + lastID1);
+			stmt.executeUpdate("DELETE FROM Users where ID = " + lastID1);
 		
 			con.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	
 
 }
